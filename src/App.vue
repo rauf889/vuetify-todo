ce9524d9252bf5a99d84309c37e4780fa2089423
rauf889 <ryagubov889@gmail.com>
2021-10-21 16:29:49 +0300

    new

---
+++ src/App.vue
@@ -0,0 +1,114 @@
+<template>
+  <v-app id="inspire">
+    <v-navigation-drawer 
+    v-model="drawer"
+    :mobile-breakpoint="768"
+    app>
+      <v-img
+      class="pa-4 pt-6"
+        src="https://picsum.photos/1920/1080?random"
+        height="170"
+        gradient="to top right, rgba(19,84,122,.5), rgba(128,208,199,.8)"
+      >
+      <v-avatar size="70" class="mb-2">
+      <img
+        src="https://cdn.vuetifyjs.com/images/john.jpg"
+        alt="John D"
+      >
+      </v-avatar>
+      <div class="white--text text-subtitle-1 font-weight-bold">John D</div>
+      <div class="white--text text-subtitle-2">john__d</div>
+      </v-img>
+
+      <v-list
+        dense
+        nav
+      >
+        <v-list-item
+          v-for="item in items"
+          :key="item.title"
+          :to="item.to"
+          link
+        >
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar
+      app
+      color="primary"
+      dark
+      src="https://picsum.photos/1920/1080?random"
+      prominent
+      :height="$route.path === '/' ? '238' : '170'"
+    >
+    <template v-slot:img="{ props }">
+      <v-img
+        v-bind="props"
+        gradient="to top right, rgba(19,84,122,.5), rgba(128,208,199,.8)"
+      ></v-img>
+    </template>
+
+    <v-container class="header-container pa-0">
+      <v-row>
+          <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+          <v-spacer></v-spacer>
+          <search/>
+      </v-row>
+
+      <v-row>
+        <v-toolbar-title class="text-h4 ml-4">
+          {{ $store.state.appTitle }}
+        </v-toolbar-title>
+      </v-row>
+      <v-row>
+        <live-date-time />
+      </v-row>
+      <v-row v-if="$route.path === '/'">
+          <field-add-task />
+      </v-row>
+    </v-container>
+
+    </v-app-bar>
+
+    <v-main>
+      <router-view></router-view>
+      <snackbar/>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import Snackbar from './components/Shared/Snackbar.vue'
+  export default {
+    data: () => ({
+       drawer: true,
+        items: [
+          { title: 'Todo', icon: 'mdi-format-list-checks', to :'/' },
+          { title: 'About', icon: 'mdi-help-box', to :'/about' },
+        ],
+       }),
+       mounted(){
+         this.$store.dispatch('getTasks')
+       },
+        components:{
+        'search': require('@/components/Tools/Search.vue').default,
+        'live-date-time': require('@/components/Tools/LiveDateTime.vue').default,
+        'field-add-task': require('@/components/Todo/FieldAddTask.vue').default,
+        'snackbar': require('@/components/Shared/Snackbar.vue').default,
+      },
+}
+</script>
+
+<style lang-"sass">
+  .header-container{
+    max-width: none !important
+  }
+</style>
---
+++ src/views/About.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="about" pa-6>
+    <h1>About {{ $store.state.appTitle }}</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+       Quisquam dicta ea sit quos minus omnis laboriosam, quo dolorum consequuntur ipsam voluptatem,
+       ipsa quae voluptates adipisci iste. Repudiandae eligendi magnam nam.</p>
+       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+       Quisquam dicta ea sit quos minus omnis laboriosam, quo dolorum consequuntur ipsam voluptatem,
+       ipsa quae voluptates adipisci iste. Repudiandae eligendi magnam nam.</p>
+       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+       Quisquam dicta ea sit quos minus omnis laboriosam, quo dolorum consequuntur ipsam voluptatem,
+       ipsa quae voluptates adipisci iste. Repudiandae eligendi magnam nam.</p>
+  </div>
+</template>
